1b10c7a40de72c721fcfbb0ac2333462cdd8d750
choeseongwoni40-prog <choeseongwoni40@gmail.com>
2026-01-05 20:09:57 +0100

    Update page.php

--- page.php
+++ page.php
@@ -1,19 +1,32 @@
 <?php get_header(); ?>
 
-<?php
-while (have_posts()) : the_post();
-    ?>
-    <article id="page-<?php the_ID(); ?>" <?php post_class('single-post'); ?>>
-        <header class="entry-header">
-            <h1 class="entry-title"><?php the_title(); ?></h1>
-        </header>
+<?php while (have_posts()): the_post(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class('single-post'); ?>>
         
-        <div class="entry-content" style="margin-top: 30px;">
+        <h1><?php the_title(); ?></h1>
+        
+        <?php 
+        $display_code = get_option('rm_display_code', '');
+        if (!empty($display_code)): 
+        ?>
+        <div class="ad-container">
+            <div class="ad-label">Advertisement</div>
+            <?php echo $display_code; ?>
+        </div>
+        <?php endif; ?>
+        
+        <div class="post-body">
             <?php the_content(); ?>
         </div>
+        
+        <?php if (!empty($display_code)): ?>
+        <div class="ad-container">
+            <div class="ad-label">Advertisement</div>
+            <?php echo $display_code; ?>
+        </div>
+        <?php endif; ?>
+        
     </article>
-    <?php
-endwhile;
-?>
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
