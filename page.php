aad3c1e12b5ff91cf956a0a4335a38eb9b85d634
choeseongwoni40-prog <choeseongwoni40@gmail.com>
2026-01-05 19:48:38 +0100

    Update page.php

--- page.php
+++ page.php
@@ -1,35 +1,19 @@
 <?php get_header(); ?>
 
-<div class="content-wrapper">
-    <main class="main-content">
-        <?php
-        while (have_posts()) : the_post();
-        ?>
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="page-header">
-                    <h1 class="page-title"><?php the_title(); ?></h1>
-                </header>
-
-                <?php if (has_post_thumbnail()) : ?>
-                    <div class="page-thumbnail">
-                        <?php the_post_thumbnail('large'); ?>
-                    </div>
-                <?php endif; ?>
-
-                <div class="page-content">
-                    <?php the_content(); ?>
-                </div>
-
-                <?php
-                if (comments_open() || get_comments_number()) :
-                    comments_template();
-                endif;
-                ?>
-            </article>
-        <?php endwhile; ?>
-    </main>
-
-    <?php get_sidebar(); ?>
-</div>
+<?php
+while (have_posts()) : the_post();
+    ?>
+    <article id="page-<?php the_ID(); ?>" <?php post_class('single-post'); ?>>
+        <header class="entry-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+        </header>
+        
+        <div class="entry-content" style="margin-top: 30px;">
+            <?php the_content(); ?>
+        </div>
+    </article>
+    <?php
+endwhile;
+?>
 
 <?php get_footer(); ?>
